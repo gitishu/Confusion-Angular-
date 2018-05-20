aca24d020812153e8e12b60e23009bfee44b80d5
gitishu <ishmeetsingh199@gmail.com>
2018-05-20 19:07:32 +0530

    for checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';//importing this modul
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/';
 
 import { AppComponent } from './app.component';
 
