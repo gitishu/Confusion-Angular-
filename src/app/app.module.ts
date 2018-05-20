cbbda4ba28532fb2d2b6f90e4a65c4abc9042a17
gitishu <ishmeetsingh199@gmail.com>
2018-05-21 00:01:43 +0530

    rxjs issue solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 //this is the root module for the angular application..
 import { BrowserModule } from '@angular/platform-browser';//importing this module from the library mentioned.
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -13,8 +17,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [//imports property specifying those modules that needs to be imported or the modules upon which our module is dependent upon 
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,//include those modules in to the imports of the ng-module declarator 
+  	MaterialModule,
+  	FlexLayoutModule
   ],
   providers: [],//here comes the services that our module use 
   bootstrap: [AppComponent]
