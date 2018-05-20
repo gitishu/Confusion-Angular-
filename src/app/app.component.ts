bc1e98bafa0a830409b11a9551cc3ebc30522507
gitishu <ishmeetsingh199@gmail.com>
2018-05-20 18:26:15 +0530

    angular animations material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 ///file supports root component that forms part of our angular application 
-import { Component } from '@angular/core';//importing the compoennet class from the angular core library.
+import { Component } from '@angular/core';//importing the component class from the angular core library.
 
-@Component({
+@Component({//this is a decorator here that it internally takes an javascript object as an paramter here 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
-}
+  // title = "hello angular cli how you are workin"; 
+}//this component class is appended with an decorator here 
+
