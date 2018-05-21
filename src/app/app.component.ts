8b0141ccb464ac8f1a3f56c96193670d03c7cf73
gitishu <ishmeetsingh199@gmail.com>
2018-05-21 22:58:23 +0530

    menu component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 ///file supports root component that forms part of our angular application 
 import { Component } from '@angular/core';//importing the component class from the angular core library.
-
+//when we import component we have to define it as a decorator here.. 
 @Component({//this is a decorator here that it internally takes an javascript object as an paramter here 
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',//name of the template file for this particular component
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent {//here we are applying component to the class because it is nothing but a js or ts class. 
+  title = 'app';//here by using export we can import this component in to my app module
   // title = "hello angular cli how you are workin"; 
 }//this component class is appended with an decorator here 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,40 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {MaterialModule} from '@angular/material';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from '@angular/material'; 
+import {FlexLayoutModule} from '@angular/flex-layout'; 
+// flexlayout versions ke saath suppot nhi kar rha nad agar ise theek kiya to baaki errors bhut aa rhe hai
+//latest version 6 me angular/cdk-bidi module find nhi kar pa rha tha islie versions hi maine 2.0.0-beta.12
+// Package                           Version
+// -----------------------------------------------------------
+// @angular-devkit/architect         0.6.3
+// @angular-devkit/build-angular     0.6.3
+// @angular-devkit/build-optimizer   0.6.3
+// @angular-devkit/core              0.6.3
+// @angular-devkit/schematics        0.6.3
+// @angular/cdk                      2.0.0-beta.8
+// @angular/cli                      6.0.3
+// @angular/flex-layout              2.0.0-beta.10
+// @angular/material                 2.0.0-beta.8
+// @ngtools/webpack                  6.0.3
+// @schematics/angular               0.6.3
+// @schematics/update                0.6.3
+// rxjs                              6.1.0
+// typescript                        2.7.2
+// webpack                           4.8.3
+
+
+
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({//this class has the declarator associated with it which is an NgModule..
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [//imports property specifying those modules that needs to be imported or the modules upon which our module is dependent upon 
     BrowserModule,
